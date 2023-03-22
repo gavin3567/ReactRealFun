38e95c41337cdd9c154f532f753d7be8f9a04125
gavin3567 <gavinostheimer00@gmail.com>
2023-03-21 22:59:38 -0600

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <h1>Welcome to Gavin's Website</h1>
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
